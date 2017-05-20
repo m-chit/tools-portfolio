6a044f505191499fd1a776e1fccad1ff12db30f1
m-chit <chit.marta1@gmail.com>
2017-05-20 21:25:17 +0200

    dropdown in navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -6,4 +6,8 @@ import {Component} from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+  dropdownShow = false;
+  DropdownShow() {
+    return this.dropdownShow = !this.dropdownShow;
+  }
 }
