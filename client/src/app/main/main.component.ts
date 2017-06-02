eefffb4d45a811bca3842568e33473525c89553e
m-chit <chit.marta1@gmail.com>
2017-06-02 21:59:07 +0200

    navbar fix

--- client/src/app/main/main.component.ts
+++ client/src/app/main/main.component.ts
@@ -2,8 +2,7 @@ import {Component} from '@angular/core';
 
 @Component({
     selector: 'app-main',
-    templateUrl: './main.component.html',
-    styleUrls: ['./main.component.css']
+    templateUrl: './main.component.html'
 })
 export class MainComponent {
 }
